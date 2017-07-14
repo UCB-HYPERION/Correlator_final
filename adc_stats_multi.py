2b7a0d8ff569c28a1d93a530d2f37c0cfc32af57
Kara K <kara@acme1.acme.pvt>
2017-07-13 20:54:08 -0700

    updated adc_stats with better bit manipulation

--- adc_stats_multi.py
+++ adc_stats_multi.py
@@ -23,6 +23,10 @@ scale = 1024
 #scale = 362
 
 import corr, struct, numpy as np, matplotlib.pyplot as plt, time
+
+def reinterpret(v):
+    return struct.unpack('b',struct.pack('B',v))[0]
+
 s = corr.katcp_wrapper.FpgaClient(host,7147,timeout = 10)
 time.sleep(1)
 s.write_int('antenna', antenna)
@@ -41,44 +45,47 @@ adc_stats = s.snapshot_get('adc_stats',man_trig=True,man_valid=True)
 stats = struct.unpack('>256b',adc_stats['data'])
 stats = np.asarray(stats)
 prequant_data = s.snapshot_get('prequant',man_trig=True,man_valid=True)
-preq = struct.unpack('>256q',prequant_data['data'])
-npreq = []
-for each in preq:
-    if each > 2**35-1:
-        each = -(2**36-each)
-    npreq.append(each)
-    print "%o" % each, type(each)
-preq = npreq
-preq = np.asarray(preq)
-preq_top = preq >> 18 
-preq_bottom = 0x3FFFF & preq
+preq = struct.unpack('>512q',prequant_data['data'])
 postquant_data = s.snapshot_get('postquant',man_trig=True,man_valid=True)
-postq = struct.unpack('>256b',postquant_data['data'])
+postq = struct.unpack('>512B',postquant_data['data'])
+
+# move bits around to make numbers readable in python
+tpreq = []
+bpreq = []
+preq_rms = 0
+for each in preq:
+    top = (each >> 18) & 0x3FFFF
+    if top > 2**35-1:
+        top = -(2**36-top)
+    tpreq.append(top)
+    bot = each & 0x3FFFF
+    if bot > 2**35-1:
+        bot = -(2**36-bot)
+    bpreq.append(bot)
+    preq_rms += top**2 + bot**2
+preq_real = np.asarray(tpreq)
+preq_imag = np.asarray(bpreq)
 tpostq = []
 bpostq = []
 postq_rms = 0
 for each in postq:
-    top = (each >> 4) & 0x0F
-    if top > 2**3-1:
-        top = -(2**4-top)
+    top = reinterpret(each & 0xF0) >> 4
     tpostq.append(top)
-    bot = each & 0x0F
-    if bot > 2**3-1:
-        bot = -(2**4-bot)
+    bot = reinterpret((each & 0x0F) << 4) >> 4
     bpostq.append(bot)
     postq_rms += top**2 + bot**2
-real = np.asarray(tpostq)
-imag = np.asarray(bpostq)
+postq_real = np.asarray(tpostq)
+postq_imag = np.asarray(bpostq)
 
 rms = np.sqrt(np.mean(np.square(stats)))
 print "Hey this one is the ADC rms"
 print rms
 
-preq_rms = np.sqrt(np.mean(np.square(preq)))
+preq_rms = np.sqrt(1.0*preq_rms/len(preq_real))
 print "Hey this one is the prequantization rms"
 print preq_rms
 
-postq_rms = np.sqrt(1.0*postq_rms/len(real))
+postq_rms = np.sqrt(1.0*postq_rms/len(postq_real))
 print "Hey this one is the postquantization rms"
 print postq_rms
 
@@ -96,7 +103,8 @@ plt.title("Histogram with 256 bins")
 plt.figure(3)
 title = 'Pre-Quantization Data: Antenna {i}'.format(i=antenna)
 plt.title(title)
-plt.plot(preq,'k')
+plt.plot(preq_real,'r', label='real')
+plt.plot(preq_imag,'b', label='imag')
 #plt.axis([0,256,-68719476736,68719476735])
 plt.grid(True)
 
@@ -107,8 +115,8 @@ plt.title("Histogram with 256 bins")
 plt.figure(5)
 title = 'Post-Quantization Data: Antenna {i}'.format(i=antenna)
 plt.title(title)
-plt.plot(real,'r', label='real')
-plt.plot(imag,'b', label='imag')
+plt.plot(postq_real,'r', label='real')
+plt.plot(postq_imag,'b', label='imag')
 plt.legend()
 #plt.axis([0,256,-136,135])
 plt.grid(True)
